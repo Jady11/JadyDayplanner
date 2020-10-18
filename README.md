54f04c2c568a021b7a3fb4445b3d8d57ea7bc113
Jady Tippetts <jady11@gmail.com>
2020-10-17 22:43:34 -0600

    updated README file with a screenshot and link to github.  updated code in the js file and cleaned up a few things.  having trouble with localstorage.

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 *Jady Tippetts Homework #5--Day Planner*
 
 
+/Users/jadytippetts/Documents/bootcamp/HOMEWORK/JadyDayplanner/Screen Shot 2020-10-17 at 10.20.50 PM (2).png
--- script.js
+++ script.js
@@ -1,50 +1,55 @@
+$(document).ready(function() {
+    $("#currentDay").text(moment().format("MMMM Do YYYY")); 
+
+})
+
 // create variables for timeblocks with text area
-var textAreaVal9 = localStorage.getItem("#hour-9");
-var textAreaVal10 = localStorage.getItem("#hour-10");
-var textAreaVal11 = localStorage.getItem("#hour-11");
-var textAreaVal12 = localStorage.getItem("#hour-12");
-var textAreaVal1 = localStorage.getItem("#hour-1");
-var textAreaVal2 = localStorage.getItem("#hour-2");
-var textAreaVal3 = localStorage.getItem("#hour-3");
-var textAreaVal4 = localStorage.getItem("#hour-4");
-var textAreaVal5 = localStorage.getItem("#hour-5");
+var textAreaVal9 = localStorage.getItem("#9AM");
+var textAreaVal10 = localStorage.getItem("#10AM");
+var textAreaVal11 = localStorage.getItem("#11AM");
+var textAreaVal12 = localStorage.getItem("#12PM");
+var textAreaVal1 = localStorage.getItem("#1PM");
+var textAreaVal2 = localStorage.getItem("#2PM");
+var textAreaVal3 = localStorage.getItem("#3PM");
+var textAreaVal4 = localStorage.getItem("#4PM");
+var textAreaVal5 = localStorage.getItem("#5PM");
 
-var textArea9 = $("#hour-9");
-var textArea10 = $("#hour-10");
-var textArea11 = $("#hour-11");
-var textArea12 = $("#hour-12");
-var textArea1 = $("#hour-1");
-var textArea2 = $("#hour-2");
-var textArea3 = $("#hour-3");
-var textArea4 = $("#hour-4");
-var textArea5 = $("#hour-5");
+var textArea9 = $("#9AM");
+var textArea10 = $("#10AM");
+var textArea11 = $("#11AM");
+var textArea12 = $("#12PM");
+var textArea1 = $("#1PM");
+var textArea2 = $("#2PM");
+var textArea3 = $("#3PM");
+var textArea4 = $("#4PM");
+var textArea5 = $("#5PM");
 
-textArea9.val(textAreaVal9);
-textArea10.val(textAreaVal10);
-textArea11.val(textAreaVal11);
-textArea12.val(textAreaVal12);
-textArea1.val(textAreaVal1);
-textArea2.val(textAreaVal2);
-textArea3.val(textAreaVal3);
-textArea4.val(textAreaVal4);
-textArea5.val(textAreaVal5);
+// textArea9.val(textAreaVal9);
+// textArea10.val(textAreaVal10);
+// textArea11.val(textAreaVal11);
+// textArea12.val(textAreaVal12);
+// textArea1.val(textAreaVal1);
+// textArea2.val(textAreaVal2);
+// textArea3.val(textAreaVal3);
+// textArea4.val(textAreaVal4);
+// textArea5.val(textAreaVal5);
 
-// $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+
 
 // past present future block color change
 // moment.com?
-var date = moment().format("MMM Do YY");  
+var hour = ("9AM","10AM","11AM","12PM","1PM","2PM","3PM","4PM","5PM");
+
+var date = moment().format("MMM Do YYYY");  
     console.log(date);
 var currentHour = moment().format('LT');
     console.log(currentHour);
 function time() {
 
-
 $("#currentDay").text(date);
 
 $(".time-block").each(function () {
-  
-    if(hour < currentHour) {
+  if(hour < currentHour) {
         $(this).addClass("past");
     } else if (hour === currentHour) {
         $(this) .removeClass("past");
@@ -55,16 +60,27 @@ $(".time-block").each(function () {
         $(this).addClass("future");
     }
     });
-};
-time();
-$(".description").on("click", function() {
+}
 
-});
+time();
+
+$("9AM .description").val(localStorage.getItem("9AM"));
+$("10AM .description").val(localStorage.getItem("10AM"));
+$("11AM .description").val(localStorage.getItem("11AM"));
+$("12PM .description").val(localStorage.getItem("12PM"));
+$("1PM .description").val(localStorage.getItem("1PM"));
+$("2PM .description").val(localStorage.getItem("2PM"));
+$("3PM .description").val(localStorage.getItem("3PM"));
+$("4PM .description").val(localStorage.getItem("4PM"));
+$("5PM .description").val(localStorage.getItem("5PM"));
+
 $(".saveBtn").on("click", function() {
+    var value= $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+    localStorage.setItem(value, time);
+
 
 })
-
-
 
 
 // add current date function
